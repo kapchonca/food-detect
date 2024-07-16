d0deafc96d96f64138b716ff8420949637e71ec7
Poptsov Alexander <147101548+kapchonca@users.noreply.github.com>
2024-07-16 09:45:44 +0300

    feat: Add YOLO predict logic
    Uploaded images are stored in the folder 'uploads'. Images after YOLO prediction are stored in 'processed/predict'.

--- fooddetect/detect/views.py
+++ fooddetect/detect/views.py
@@ -2,6 +2,7 @@ import os
 from django.shortcuts import render
 from fooddetect.settings import BASE_DIR
 from detect.forms import UploadFileForm
+from ultralytics import YOLO
 
 # Create your views here.
 
@@ -13,7 +14,17 @@ def handle_uploaded_file(f):
     with open(os.path.join(upload_dir, f.name), "wb+") as destination:
         for chunk in f.chunks():
             destination.write(chunk)
+    
+    process_image(f.name)
 
+def process_image(file_name):
+    upload_dir = BASE_DIR / 'processed/'
+    if not os.path.exists(upload_dir):
+        os.makedirs(upload_dir)
+
+    model = YOLO(BASE_DIR / 'models/detect.pt')
+    results = model(BASE_DIR / f'uploads/{file_name}', save=True, project=BASE_DIR / 'processed', exist_ok=True)
+    
 def index(request):
     if request.method == 'POST':
         form = UploadFileForm(request.POST, request.FILES)
