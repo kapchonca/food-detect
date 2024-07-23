6ff81b2724a23bbd408d5ea8d961268d057f8b49
Poptsov Alexander <147101548+kapchonca@users.noreply.github.com>
2024-07-23 09:42:34 +0300

    refactor: Extract detection confidence instead of coordinates

--- fooddetect/models/detect.py
+++ fooddetect/models/detect.py
@@ -1,6 +1,6 @@
 import os
-from fooddetect.settings import BASE_DIR, MEDIA_ROOT
 from ultralytics import YOLO
+from fooddetect.settings import BASE_DIR, MEDIA_ROOT
 
 def handle_uploaded_file(f):
     upload_dir = MEDIA_ROOT / 'uploads/'
@@ -14,27 +14,48 @@ def handle_uploaded_file(f):
     process_image(f.name)
     return f.name
 
+def img_to_txt_filename(file_name):
+    file_extension = file_name.rfind('.')
+    return file_name[:file_extension] + '.txt'
+
 def process_image(file_name):
     upload_dir = MEDIA_ROOT / 'processed/'
     if not os.path.exists(upload_dir):
         os.makedirs(upload_dir)
 
     model = YOLO(BASE_DIR / 'models/detect.pt')
-    model(MEDIA_ROOT / f'uploads/{file_name}', save=True, save_txt=True, project=MEDIA_ROOT / 'processed', exist_ok=True)
+    detections = model(MEDIA_ROOT / f'uploads/{file_name}', save=True, project=MEDIA_ROOT / 'processed', exist_ok=True, conf=0.1)
+    process_detections(detections, file_name)
+
+def process_detections(detections, file_name):
+    top_detections = {}
+
+    for detection in detections:
+        for box in detection.boxes:
+            class_id = int(box.cls)
+            confidence = float(box.conf)
+
+            if class_id not in top_detections:
+                top_detections[class_id] = 0
+
+            top_detections[class_id] = max(confidence, top_detections[class_id])
+            
+    file_name = img_to_txt_filename(file_name)
+
+    with open(MEDIA_ROOT / 'processed/predict/labels' / file_name, 'w') as f:
+        for class_id, confidence in top_detections.items():
+            f.write(f"{class_id} {confidence}")
+            f.write("\n")
 
 def extract_classes(file_name):
-    file_extension = file_name.rfind('.')
-    file_name = file_name[:file_extension] + '.txt'
-    with open(BASE_DIR / 'models' / 'labels.txt') as file_labels:
-        labels = file_labels.read().split('\n')
+    file_name = img_to_txt_filename(file_name)
 
     if not(os.path.exists(MEDIA_ROOT / 'processed' / 'predict' / 'labels' / file_name)):
-        return {'non-food': ['0', '0', '0', '0']}
+        return {'non-food': '0'}
     
     with open(MEDIA_ROOT / 'processed' / 'predict' / 'labels' / file_name) as file_predictions:
         classes_raw = file_predictions.read().split('\n')
     classes_list = [list(i.split(' ')) for i in classes_raw]
-    classes_dict = { i[0] : i[1:] for i in classes_list if i[0]}
-    classes = {labels[int(i)] : classes_dict[i] for i in classes_dict}
+    classes = { i[0] : i[1] for i in classes_list if i[0]}
 
     return classes
